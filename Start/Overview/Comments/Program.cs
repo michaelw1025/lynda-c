8f398fb0a2431d42776582dfbf2bbd727f09413a
michaelw1025 <michaelw1025@gmail.com>
2021-06-30 00:39:21 -0500

    first commit

---
+++ Start/Overview/Comments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML comments help provide documentation
+        /// <summary>
+        /// This is the main sample application function
+        /// </summary>
+        /// <param name='args'>An array of string arguments from the command line</param>
+        /// <returns>
+        /// No return value
+        /// </returns>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Single line comment
+
+            /*
+            ...
+            ...
+            Multi-line comments
+            ...
+            ...
+            */
+
+        }
+    }
+}
---
+++ Start/Overview/Operators/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            int x=10, y=5;
+            string a="abcd", b="efgh";
+
+            // Basic math
+            // Console.WriteLine((x/y)*x);
+            // Console.WriteLine(a+b);
+
+            // Increment/Decrement
+            x++;
+            y--;
+            // Console.WriteLine(x);
+            // Console.WriteLine(y);
+
+            // Shorthand 
+            // a = a + b
+            // a += b;
+            // Console.WriteLine(a);
+
+            // Logical operators
+            // && and operator  
+            // Console.WriteLine(x > y && y >= 5);
+
+            // || or operator  
+            // Console.WriteLine(x > y || y >= 5);
+
+            // null-coalescing operators
+            string str = null;
+            // The ?? operator uses left operand if not null, or the right one if it is 
+            Console.WriteLine(str ?? "unkown");
+            // The ??= operator assigns the right operand if the left one is null
+            str ??= "New String";
+            Console.WriteLine(str);
+        }
+    }
+}
