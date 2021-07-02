78d128b878584f33e5ef21b4684a541ee71538ff
michaelw1025 <michaelw1025@gmail.com>
2021-07-01 22:54:55 -0500

    continue

---
+++ Start/Program Flow/Conditional-switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Conditional_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theVal = 66;
+
+            // The switch statment
+            switch (theVal){
+                case 50:
+                    Console.WriteLine("theVal is 50");
+                    break;
+                case 51:
+                    Console.WriteLine("theVal is 51");
+                    break;
+                case 52:
+                    Console.WriteLine("theVal is 52");
+                    break;
+                case 53:
+                case 54:
+                case 55:
+                    Console.WriteLine("theVal is between 53 and 55");
+                    break;
+                default:
+                    Console.WriteLine("theVal is something else");
+                    break;
+            }
+        }
+    }
+}
---
+++ Start/Program Flow/WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "";
+
+            // Basic while loop executes while the gate condition is true
+            // while (inputStr != "exit"){
+            //     inputStr = Console.ReadLine();
+            //     Console.WriteLine("You entered: {0}", inputStr);
+            // }
+
+            // The do-while loop always executes at least one time
+            do {
+                inputStr = Console.ReadLine();
+                Console.WriteLine("You entered: {0}", inputStr);
+            }while(inputStr != "exit");
+        }
+    }
+}
